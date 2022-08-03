ea5faa6806bff9ca3ec146653967c19f7089e025
ricardoafroes <ricardo_a_froes@yahoo.com.br>
2022-08-03 15:39:28 -0300

    Desenvolvimento dos layout de listagem, e detalhes de partidas

---
+++ app/src/main/java/projetos/f1/simulator/DetailActivity.kt
@@ -0,0 +1,19 @@
+package projetos.f1.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projetos.f1.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
